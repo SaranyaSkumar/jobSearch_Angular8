61ae33b5b183a6d4190913e71d3f66f9501688a9
saranya <saranya.kumar@netmeds.com>
2020-02-11 11:25:44 +0530

    console

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,9 @@ module.exports = app => {
 
     app.post('/register', function (req, res) {
         let postData=req.body;
+        console.log("postData", postData)
         users.create(postData).then(data=>{
+            console.log("datadata",data)
             res.status(200).json({message: 'success', data: postData})
         }).catch((e)=>{
             res.status(400).json({message: 'error', data: e})
